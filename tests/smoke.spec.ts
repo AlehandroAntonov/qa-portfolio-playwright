4ef04f2908dbc463ad85e4606f1c603b7f8c23ce
AlehandroAntono <Antonovmainmail@gmail.com>
2026-07-06 12:08:59 +0500

    feat: add initial smoke tests for SauceDemo

---
+++ tests/smoke.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect} from '@playwright/test';
+
+test ('home page opens successfully', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await expect(page).toHaveTitle(/Swag Labs/);
+});
+
+test ('login form is visible', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible();
+    await expect(page.getByRole('textbox', { name: 'Password' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+});
+
+test ('successful login', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.getByRole('textbox',  { name: 'Username' }).fill('standard_user');
+    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login '}).click();
+    await expect(page).toHaveURL(/inventory/);
+});
