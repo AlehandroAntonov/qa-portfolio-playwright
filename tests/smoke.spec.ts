3cf2015d11a9bab489c97a28be59187b098d8746
AlehandroAntono <Antonovmainmail@gmail.com>
2026-07-06 17:58:14 +0500

    refactor: implement Page Object Model for LoginPage

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,29 +1,30 @@
 import { test, expect} from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
 
 test ('home page opens successfully', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com');
-    await expect(page).toHaveTitle(/Swag Labs/);
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await expect(page).toHaveTitle(/Swag Labs/)
 });
 
 test ('login form is visible', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com');
-    await expect(page.getByRole('textbox', { name: 'Username' })).toBeVisible();
-    await expect(page.getByRole('textbox', { name: 'Password' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await expect(loginPage.usernameInput).toBeVisible();
+    await expect(loginPage.passwordInput).toBeVisible();
+    await expect(loginPage.loginButton).toBeVisible();
 });
 
 test ('successful login', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com');
-    await page.getByRole('textbox',  { name: 'Username' }).fill('standard_user');
-    await page.getByRole('textbox', { name: 'Password' }).fill('secret_sauce');
-    await page.getByRole('button', { name: 'Login '}).click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauce');
     await expect(page).toHaveURL(/inventory/);
 });
 
 test ('failed login show error message', async ({ page })=> {
-    await page.goto('https://www.saucedemo.com');
-    await page.getByRole('textbox', { name: 'Username' }).fill('wrong_user');
-    await page.getByRole('textbox', { name: 'Password' }).fill('wrong_password');
-    await page.getByRole('button', {name: 'Login' }).click();
-    await expect(page.getByText('Epic sadface')).toBeVisible();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('wrong_user', 'wrong_password');
+    await expect(loginPage.errorMessage).toBeVisible();
 });
