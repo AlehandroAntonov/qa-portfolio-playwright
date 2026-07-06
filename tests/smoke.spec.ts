8a920bacbf597ad12dc78ebc2071cd9db6273b7c
AlehandroAntono <Antonovmainmail@gmail.com>
2026-07-06 14:17:30 +0500

    failed login test

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -19,3 +19,11 @@ test ('successful login', async ({ page }) => {
     await page.getByRole('button', { name: 'Login '}).click();
     await expect(page).toHaveURL(/inventory/);
 });
+
+test ('failed login show error message', async ({ page })=> {
+    await page.goto('https://www.saucedemo.com');
+    await page.getByRole('textbox', { name: 'Username' }).fill('wrong_user');
+    await page.getByRole('textbox', { name: 'Password' }).fill('wrong_password');
+    await page.getByRole('button', {name: 'Login' }).click();
+    await expect(page.getByText('Epic sadface')).toBeVisible();
+});
